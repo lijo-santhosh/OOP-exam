f6aae2c0701889383c1fc1e45cf7d3cde0dd810b
lijo-santhosh <109999642+lijo-santhosh@users.noreply.github.com>
2022-10-26 14:10:36 +1030

    Add files via upload

---
+++ Employee.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "Employee.h"
+
+using namespace std;
+
+
+void Employee::takeABreak(int mins) {
+    if (energyLevel < 100) {
+    energyLevel = energyLevel + (mins*(2));
+    }
+}
