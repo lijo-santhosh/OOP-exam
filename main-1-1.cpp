4a6a1a46342b92d3adc0f90c6fd50a51ea548e6f
lijo-santhosh <109999642+lijo-santhosh@users.noreply.github.com>
2022-10-26 13:46:39 +1030

    Add files via upload

---
+++ main-1-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Employee.h"
+
+using namespace std;
+
+int main() {
+
+    Employee e1;
+
+    E1.get_payRate
+
+    cout << "Pay rate: " << worker.get_payRate() << endl;
+
+    return 0;
+}
