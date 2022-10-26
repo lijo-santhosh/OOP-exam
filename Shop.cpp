b384203685b7c18efddf6fff33581c254b8aa9ce
lijo-santhosh <109999642+lijo-santhosh@users.noreply.github.com>
2022-10-26 14:50:14 +1030

    Add files via upload

---
+++ Shop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include "Casual.h"
+
+using namespace std;
+
+
+void Employee::takeABreak(int mins) {
+    if (energyLevel < 100) {
+        //uhfuhehfihei
+        //fij3f/uheijfhief/kf
+
+        //ndiwdkwmfwfwm
+    energyLevel = energyLevel + (mins*(2)+2+2+2+3);
+    }
+}
